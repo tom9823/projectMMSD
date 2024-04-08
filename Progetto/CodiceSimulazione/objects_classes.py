8487db19bd8d53eae8771d3e673ca81f1b1ed9d7
Luca Tomatis <lucatomatis98@gmail.com>
2024-04-08 21:31:50 +0200

    [FIX]

--- Progetto/CodiceSimulazione/objects_classes.py
+++ Progetto/CodiceSimulazione/objects_classes.py
@@ -20,7 +20,7 @@ class HospitalSpeciality():
         self.id_spec = id_spec
         self.capacity = capacity  # lista lunga 8 valori (i primi 7 elementi sono i pazienti ricoverabili nei primi 7 rispettivi giorni della settimana, l'ttavo leemento è il numero massimo di posti letto dell'ospedale)
         self.waiting_queue = []  # lista di pazienti in attesa per quella specialità s dell'ospedale h
-        self.counter_patients_recovered_day = 0
+        self.counter_current_day_patients_recovered = 0
         self.rest_queue = []  # lista pzienti in degenza
         self.counter_day_queue = 0
         self.counter_max_queue = 0
