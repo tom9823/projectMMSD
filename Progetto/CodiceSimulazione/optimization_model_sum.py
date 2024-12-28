538746fa150b12117698bcc7fac5d6a30d450d34
Luca Tomatis <lucatomatis98gmail.com>
2024-12-28 12:22:01 +0100

    fix model

--- Progetto/CodiceSimulazione/optimization_model_sum.py
+++ Progetto/CodiceSimulazione/optimization_model_sum.py
@@ -35,36 +35,33 @@ d = distanze con i nuovi ospedali
 
 
 # Variabile obbiettivo
-def obj_expression_norm_1(model):
-    return pyo.summation(model.delta)
+def obj_expression_norm_1(m):
+    return pyo.summation(m.delta)
 
+def obj_expression_norm_2(m):
+    return pyo.sqrt(sum(m.delta[p] ** 2 for p in m.P))
 
-def obj_expression_norm_2(model):
-    return pyo.sqrt(sum(model.delta[p] ** 2 for p in model.P))
-
-
-def obj_expression_norm_inf(model):
-    return max(model.delta)
-
+def obj_expression_norm_inf(m):
+    return max(m.delta)
 
 # Vincoli
-def patient_assigned_to_only_one_hospital(model, p):
-    return sum(model.x[p, h] for h in model.H) == 1
+def patient_in_only_one_hospital(m, p):
+    return sum(m.x[p, h] for h in m.H) == 1
 
 
-def patients_redistribution(model, h):
-    return sum(model.l[p] * model.x[p, h] for p in model.P) <= model.gamma[h]
+def patients_redistribution(m, h):
+    return sum(m.l[p] * m.x[p, h] for p in m.P) <= m.gamma[h]
 
 
-def discomfort_calculation(model, p, h):
-    return model.delta[p] >= (model.d[p, h] - model.m[p]) * model.x[p, h]
+def discomfort_calculation(m, p, h):
+    return m.delta[p] >= (m.d[p, h] - m.m[p]) * m.x[p, h]
 
-
-def dont_exceed_maximum_discomfort(model, p):
-    return max(model.delta) >= model.delta[p]
+def dont_exceed_maximum_discomfort(m, p):
+    return max(m.delta) >= m.delta[p]
 
 
 def create_model(data, solver, time_limit, optimizer_model_type):
+    # print(f'Inizio modello')
     model = pyo.AbstractModel()
     opt = solvers.SolverFactory(solver)
     if solver == 'cplex':
@@ -73,7 +70,6 @@ def create_model(data, solver, time_limit, optimizer_model_type):
         opt.options['tmlim'] = time_limit
     elif solver == 'gurobi':
         opt.options['TimeLimit'] = time_limit
-
     # Parametri
     model.P = pyo.Set(within=pyo.NonNegativeIntegers)
     model.H = pyo.Set(within=pyo.NonNegativeIntegers)
@@ -81,12 +77,9 @@ def create_model(data, solver, time_limit, optimizer_model_type):
     model.gamma = pyo.Param(model.H)
     model.m = pyo.Param(model.P)
     model.d = pyo.Param(model.P, model.H)
-
     # Variabili
     model.x = pyo.Var(model.P, model.H, domain=pyo.Binary)
     model.delta = pyo.Var(model.P, domain=pyo.NonNegativeReals)
-    model.q = pyo.Var(model.P, domain=pyo.NonNegativeReals)
-
     #Funzione obiettivo
     if optimizer_model_type == OptimizerModelType.NORM_1:
         model.OBJ = pyo.Objective(rule=obj_expression_norm_1)
@@ -94,25 +87,24 @@ def create_model(data, solver, time_limit, optimizer_model_type):
         model.OBJ = pyo.Objective(rule=obj_expression_norm_2)
     elif optimizer_model_type == OptimizerModelType.NORM_INF:
         model.OBJ = pyo.Objective(rule=obj_expression_norm_inf)
-
-    # Vincoli
-    model.PatientInOnlyOneHospital = pyo.Constraint(model.P, rule=patient_assigned_to_only_one_hospital)
+    #Vincoli
+    model.PatientInOnlyOneHospital = pyo.Constraint(model.P, rule=patient_in_only_one_hospital)
     model.PatientsRedistribution = pyo.Constraint(model.H, rule=patients_redistribution)
     model.DiscomfortCalculation = pyo.Constraint(model.P, model.H, rule=discomfort_calculation)
+
     if optimizer_model_type == OptimizerModelType.NORM_INF:
         model.DontExceedMaximumDiscomfort = pyo.Constraint(model.P, rule=dont_exceed_maximum_discomfort)
-    model.OBJ = pyo.Objective(rule=obj_expression_norm_inf)
     model_instance = model.create_instance(data)
-
     # model_instance.pprint()
-    # Solving del modello
-    results = opt.solve(model_instance)
+    results = opt.solve(model_instance)  # Solving del modello
 
     if (results.solver.status == SolverStatus.ok) and (
             results.solver.termination_condition == TerminationCondition.optimal):
         return results, model_instance
-    elif results.solver.termination_condition == TerminationCondition.infeasible:
+    elif (results.solver.termination_condition == TerminationCondition.infeasible):
+        # print(f'Modello infeasible')
         return results, model_instance
     else:
         print(f'Solver Status: {results.solver.status}')
+    print(f'Fine modello')
     return results, model_instance
